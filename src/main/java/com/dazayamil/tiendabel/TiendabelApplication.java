9a61c92178682ac84e37b70e4dab898f45bc9806
daza yamil <dazayamil07@gmail.com>
2026-03-04 17:41:12 -0300

    chore: initial project structure

---
+++ src/main/java/com/dazayamil/tiendabel/TiendabelApplication.java
@@ -0,0 +1,13 @@
+package com.dazayamil.tiendabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dazayamil/tiendabel/TiendabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazayamil.tiendabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
