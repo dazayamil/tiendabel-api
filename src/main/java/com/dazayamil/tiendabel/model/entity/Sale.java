21c78a2addea27ba770fe2dd35c5301f25ecc9e0
daza yamil <dazayamil07@gmail.com>
2026-03-09 22:59:42 -0300

    feat: add Sale and SaleItem entities

--- src/main/java/com/dazayamil/tiendabel/model/entity/Sale.java
+++ src/main/java/com/dazayamil/tiendabel/model/entity/Sale.java
@@ -1,4 +1,36 @@
 package com.dazayamil.tiendabel.model.entity;
 
+import com.dazayamil.tiendabel.model.enums.Payment;
+import com.dazayamil.tiendabel.model.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+@Entity
+@Table(name = "sale")
+@Data
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "create_at", nullable = false)
+    private LocalDate createAt;
+
+    @Column(name = "payment_method", nullable = false)
+    private Payment paymentMethod;
+
+    @Column(name = "total_amount", nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    private Status status;
+
+    @OneToMany(mappedBy = "sale")
+    private List<SaleItem> items;
+
 }
