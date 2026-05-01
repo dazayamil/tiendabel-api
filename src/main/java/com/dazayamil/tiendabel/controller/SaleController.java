f877d99f8b76488f0fac70dd111b65876912c025
daza yamil <dazayamil07@gmail.com>
2026-05-01 18:03:58 -0300

    feat(sales): add enpoint to list sales

--- src/main/java/com/dazayamil/tiendabel/controller/SaleController.java
+++ src/main/java/com/dazayamil/tiendabel/controller/SaleController.java
@@ -4,10 +4,12 @@ import com.dazayamil.tiendabel.dto.request.SaleCreateRequestDTO;
 import com.dazayamil.tiendabel.dto.response.SaleResponseDTO;
 import com.dazayamil.tiendabel.service.SaleService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/sales")
@@ -18,15 +20,20 @@ public class SaleController {
         this.saleService = saleService;
     }
 
+    @GetMapping()
+    public ResponseEntity<List<SaleResponseDTO>> getAllSales(){
+        return ResponseEntity.ok(this.saleService.getAllSales());
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<SaleResponseDTO> getSaleById(@PathVariable Long id){
-        return ResponseEntity.ok(saleService.getSaleById(id));
+        return ResponseEntity.ok(this.saleService.getSaleById(id));
     }
 
     // POST /v1/sales -> 201 Create con Location header
     @PostMapping
     public ResponseEntity<SaleResponseDTO> createSale(@Valid @RequestBody SaleCreateRequestDTO request){
-        SaleResponseDTO created = saleService.createSale(request);
+        SaleResponseDTO created = this.saleService.createSale(request);
         URI location = URI.create("/v1/sales/" + created.getSaleId());
 
         return ResponseEntity.created(location).body(created);
--- src/main/java/com/dazayamil/tiendabel/mapper/SaleMapper.java
+++ src/main/java/com/dazayamil/tiendabel/mapper/SaleMapper.java
@@ -5,9 +5,12 @@ import com.dazayamil.tiendabel.model.entity.Sale;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface SaleMapper {
 
     @Mapping(source = "id", target = "saleId")
     SaleResponseDTO toResponseDTO(Sale sale);
+    List<SaleResponseDTO> toResponseDTOList (List<Sale> sales);
 }
